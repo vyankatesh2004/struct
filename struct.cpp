51f4197a0f555b3c5f6becc9985ea1566af6b9ef
vyankatesh2004 <vyankateshdnagargoje04@gmail.com>
2023-08-29 02:28:52 +0530

    code improvement

--- struct.cpp
+++ struct.cpp
@@ -6,15 +6,33 @@ struct student {
     int marks[5];
 };
 
-void bs(float arr[],int n){
+void bs(float crt[],int indArr[],int n){
+    
+    for(int i=0;i<5;i++){
+        indArr[i]=i; 
+    }
+
     while(--n){
         for(int i=0;i<n;i++){
-            if(arr[i]>arr[i+1]){
-                float c= arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=c;
+            if(crt[indArr[i]]>crt[indArr[i+1]]){
+                float c= indArr[i];
+                indArr[i]=indArr[i+1];
+                indArr[i+1]=c;
             }
         }
+    }
+}
+
+void crtInput(student s[], float crt[]){
+    // we have considered max marks this time
+    for(int i=0;i<5;i++){
+        int max=0;
+        for(int j=0;j<5;j++){
+            if(s[i].marks[j]> max){
+                max= s[i].marks[j];
+            }
+        }
+        crt[i]= max;
     }
 }
 
@@ -22,32 +40,43 @@ int main(){
 
 int n=5;
 student *s = new student[n];
-float *avg = new float[n];
+float *crt = new float[n];
+int* indArr = new int[5];
+
+//Taking the input
 
 for(int i=0;i<n;i++){
     cout<<"enter the name of "<<i+1<<" student"<<endl;
-    cin>> s[i].name;
-    avg[i]=0;
+    cin>> s[i].name;    
     cout<<"enter the marks of "<<i+1<<" student"<<endl;
     for(int j=0;j<n;j++){
-        cin>> s[i].marks[j];
-        avg[i]+=s[i].marks[j];
-    }
-    avg[i]/=n;
+        cin>> s[i].marks[j];        
+    }    
 }
 
+crtInput(s, crt);
+
+cout<<endl;
+
+// printing original criteria array
+
+for (int i = 0; i < n; i++){
+    cout<< crt[i] << " ";
+}
+cout<<endl;
+cout<<endl;
+
+bs(crt,indArr, n);
+
+// printing students name and criteria after sorting
+
+for (int i = 0; i < n; i++){
+    cout<< s[indArr[i]].name << "  ";
+}
 cout<<endl;
 
 for (int i = 0; i < n; i++){
-    cout<< avg[i] << " ";
-}
-cout<<endl;
-cout<<endl;
-
-bs(avg, n);
-
-for (int i = 0; i < n; i++){
-    cout<< avg[i] << "  ";
+    cout<< crt[indArr[i]] << "  ";
 }
 cout<<endl;
 
